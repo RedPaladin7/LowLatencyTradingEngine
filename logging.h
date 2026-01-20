cd8d31b42b38b4f197d4e7d31927f235068a7f37
ishan <ishans7325@gmail.com>
2026-01-20 13:00:20 +0530

    20th Jan 1

---
+++ logging.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include <string>
+#include <fstream>
+#include <cstdio>
+
+#include "macros.h"
+#include "lf_queue.h"
+#include "thread_utils.h"
+#include "time_utils.h"
+using namespace std;
+
+namespace Common {
+}
